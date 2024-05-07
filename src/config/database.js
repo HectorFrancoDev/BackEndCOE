a0dc88fa077fb719c7ff691dbd9f6fd13da92a3e
HectorFranco-MISO <h.franco@uniandes.edu.co>
2024-05-07 15:27:10 -0500

    Release

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dbConnection = async () => {
 
     try {
-        await mongoose.connect(process.env.MONGODB_LOCAL);
+        await mongoose.connect(process.env.MONGODB_ATLAS);
         console.log('MongoDB online');
     }
     catch (error) {
